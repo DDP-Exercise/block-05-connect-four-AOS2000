ef3af498b48677743e8f79ecbef3a720ab76c814
AOS2000 <s2510892024@fhooe.at>
2026-05-13 10:49:23 +0200

    Main

--- js/view.polished.js
+++ js/view.polished.js
@@ -12,3 +12,50 @@
 
 //TODO: Notify the player when the game is over. Make it clear how the
 //      Game ended. If it's a win, show the winning stones.
+export const polishedView = {
+    init(rows, cols) {
+        const boardElement = document.getElementById("board");
+        if (!boardElement) return;
+        boardElement.innerHTML = "";
+
+        for (let r = 0; r < rows; r++) {
+            for (let c = 0; c < cols; c++) {
+                let tile = document.createElement("div");
+                tile.id = `${r}-${c}`;
+                tile.classList.add("tile");
+                tile.dataset.col = c.toString();
+                boardElement.append(tile);
+            }
+        }
+    }
+};
+
+window.addEventListener("cf:stone-inserted", (e) => {
+    const { row, col, player } = e.detail; // Hier row (Einzahl)
+    const tile = document.getElementById(`${row}-${col}`);
+    if (tile) {
+        const className = (player.color === "Red") ? "red-player" : "yellow-player";
+        tile.classList.add(className);
+    }
+});
+
+window.addEventListener("cf:player-changed", (e) => {
+    const leftImg = document.querySelector(".left-char img");
+    const rightImg = document.querySelector(".right-char img");
+    if (!leftImg || !rightImg) return;
+
+    if (e.detail.player.firstName === "Nacho") {
+        leftImg.style.border = "5px solid Red";
+        rightImg.style.border = "none";
+    } else {
+        rightImg.style.border = "5px solid Yellow";
+        leftImg.style.border = "none";
+    }
+});
+window.addEventListener("cf:game-over", (e) => {
+    const winnerDisplay = document.getElementById("Winner");
+    if (winnerDisplay) {
+        winnerDisplay.innerText = e.detail.winner + " hat gewonnen! 🎉";
+    }
+    document.getElementById("board").style.opacity = "0.8";
+});
